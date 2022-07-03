3b6de04a334d812571ba8ce759dd716c47d58a2c
Felp056 <felipetalini5@gmail.com>
2022-07-02 21:11:36 -0300

    add tabelas

---
+++ AgroControl/tabelas/function.js
@@ -0,0 +1,24 @@
+let sementes = document.querySelector('#sementes');
+let metros = document.querySelector('#metros');
+let peso1 = document.querySelector('#peso1');
+let venda1 = document.querySelector('#venda1');
+let valor1 = document.querySelector('#valor1');
+let peso2 = document.querySelector('#peso2');
+let venda2 = document.querySelector('#venda2');
+let valor2 = document.querySelector('#valor2');
+let saca = document.querySelector('#saca');
+let venda3 = document.querySelector('#venda3');
+let compra = document.querySelector('#compra');
+let maquinario = document.querySelector('#maquinario');
+let data = document.querySelector('#data');
+let colhido = document.querySelector('#colhido');
+let gasto_insumo = document.querySelector('#gasto_insumo');
+let bruto = document.querySelector('#bruto');
+let despesas = document.querySelector('#despesas');
+let save = document.querySelector('#save');
+let linhas = document.querySelector('#linhas');
+let colunas = document.querySelector('#colunas');
+let lin = document.querySelector('#lin');
+let col = document.querySelector('#col');
+
+ 

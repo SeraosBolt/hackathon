7f37263265c3a5313c93a37a91a34ef74005449a
MAtheus Soares <math.fssoares@gmail.com>
2022-07-03 16:59:59 -0300

    Atualização LUCRO

--- AgroControl/Gastos_Lucros/scripts/gastos_lucros.js
+++ AgroControl/Gastos_Lucros/scripts/gastos_lucros.js
@@ -5,6 +5,9 @@ let btSsubtrair = document.querySelector('#subtrair')
 function abrePopUp(){
     let varWindow = window.open('../Gastos_Lucros/pagLucro/lucro.html', 'popup', "width = 520 , height = 320 , top = 300, left = 690, scrollbars = no")
 }
+function abrePopUp(){
+    let varWindow = window.open('../Gastos_Lucros/pagLucro/lucro.html', 'popup', "width = 520 , height = 320 , top = 300, left = 690, scrollbars = no")
+}
 
 
 btSomar.onclick = function(){

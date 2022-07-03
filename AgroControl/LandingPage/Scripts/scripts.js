5ea0884f25423692171bab61717c8887d60a448c
DelGrau <alanhipermassa@gmail.com>
2022-07-03 16:50:57 -0300

    Mudanças mínimas

--- AgroControl/LandingPage/Scripts/scripts.js
+++ AgroControl/LandingPage/Scripts/scripts.js
@@ -3,8 +3,6 @@ let logado = Boolean(false)
 let userName = document.querySelector("#userName");
 let user = "Nome vai aqui";
 
-import { userLogado } from "../../../loginPage/Scripts/loginPageScript";
-
 function newPopup(){ // função que abre a tela de login
     let varWindow = window.open('../../loginPage/loginPage.html','popup',"width=520, height=320, top=300, left=690, scrollbars=no ")
 }

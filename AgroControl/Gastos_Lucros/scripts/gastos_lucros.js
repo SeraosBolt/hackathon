1cad1efa35f598d84bb2d3c5aa1264b7a4ca09b8
MAtheus Soares <math.fssoares@gmail.com>
2022-07-03 17:34:50 -0300

    Ultimo commit

--- AgroControl/Gastos_Lucros/scripts/gastos_lucros.js
+++ AgroControl/Gastos_Lucros/scripts/gastos_lucros.js
@@ -1,12 +1,16 @@
 let btSomar = document.querySelector('#somar')
 let btSsubtrair = document.querySelector('#subtrair')
+let btVoltar = document.querySelector("#voltar")
 
 
 function abrePopUp(){
-    let varWindow = window.open('../Gastos_Lucros/pagLucro/lucro.html', 'popup', "width = 520 , height = 320 , top = 300, left = 690, scrollbars = no")
+    let varWindow = window.open('../Gastos_Lucros/pagLucro/lucro.html', 'popup', "width = 620 , height = 420 , top = 300, left = 690, scrollbars = no")
 }
 function abrePopUp2(){
     let varWindow = window.open('../Gastos_Lucros/pagGasto/gasto.html', 'popup', "width = 520 , height = 320 , top = 300, left = 690, scrollbars = no")
+}
+function voltar(){
+    window.location = '../LandingPage/index.html'
 }
 
 
@@ -15,4 +19,8 @@ btSomar.onclick = function(){
 }
 btSsubtrair.onclick = function(){
     abrePopUp2()
+}
+
+btVoltar.onclick = function(){
+    voltar()
 }

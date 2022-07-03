1c7ed2fd5d508e462cbffa59c24f58ed91f881da
DelGrau <alanhipermassa@gmail.com>
2022-07-03 14:31:33 -0300

    Updates no Tabelas e no login page

--- AgroControl/LandingPage/Scripts/scripts.js
+++ AgroControl/LandingPage/Scripts/scripts.js
@@ -1,5 +1,17 @@
-let p = document.querySelector("#oi")
+let body = document.querySelector("#body"); // carrega o body
+let logado = Boolean(false)
+let userName = document.querySelector("#userName");
+let user = "Nome vai aqui";
 
-function seila(){
-    p.innerHTML = "Fiadaputa"
+import { userLogado } from "../../../loginPage/Scripts/loginPageScript";
+
+function newPopup(){ // função que abre a tela de login
+    let varWindow = window.open('../../loginPage/loginPage.html','popup',"width=520, height=320, top=300, left=690, scrollbars=no ")
 }
+
+body.onload = function(){ // quando carrega o body inicia a função
+    if(logado == false){  // se 'logado' for falso abre o pop-up da tela de login
+        newPopup()
+    }
+}
+
